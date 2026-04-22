8bbabd6fc0c96e2db5e942519a9c5250060f3b75
Ilya Michlin <ilmih123@gmail.com>
2026-04-22 07:08:05 +0300

    feat(phase-1): Added data pipelines init file

---
+++ country_compare/pipelines/__init__.py
@@ -0,0 +1,26 @@
+from country_compare.pipelines.engine import PipelineEngine, run_processing_pipeline
+from country_compare.pipelines.models import (
+    AcquiredAsset,
+    ProcessingRequest,
+    ProcessingResult,
+    PublicationReport,
+    RowIssue,
+    RunMetadata,
+    SourceProcessingResult,
+    SourceSpec,
+    ValidationReport,
+)
+
+__all__ = [
+    "AcquiredAsset",
+    "PipelineEngine",
+    "ProcessingRequest",
+    "ProcessingResult",
+    "PublicationReport",
+    "RowIssue",
+    "RunMetadata",
+    "SourceProcessingResult",
+    "SourceSpec",
+    "ValidationReport",
+    "run_processing_pipeline",
+]
